7cdba123d15c11f8c19c95f620382598dae62ede
oggnimodd <arnold.ds.dev@gmail.com>
2024-11-14 06:27:45 +0000

    refactor: Adjust position when transforming to Fire Pea

--- src/lib/models/projectiles/Projectile.svelte.ts
+++ src/lib/models/projectiles/Projectile.svelte.ts
@@ -40,10 +40,20 @@ export default class Projectile {
   }
 
   transformToFirePea() {
+    const originalWidth = this.width;
+    const originalHeight = this.height;
+
     this.type = ProjectileTypes.FIRE_PEA.type;
     this.damage *= 2;
     this.image = ProjectileTypes.FIRE_PEA.image;
     this.width = ProjectileTypes.FIRE_PEA.width;
     this.height = ProjectileTypes.FIRE_PEA.height;
+
+    // Calculate the center offset and adjust the position
+    const widthOffset = (this.width - originalWidth) / 2;
+    const heightOffset = (this.height - originalHeight) / 2;
+
+    this.x -= widthOffset;
+    this.y -= heightOffset;
   }
 }
