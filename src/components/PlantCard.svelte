b4785d3356860066a39d418981adfd9059a23da1
oggnimodd <arnold.ds.dev@gmail.com>
2024-11-21 04:06:19 +0000

    feat: Add plant image to PlantCard

--- src/components/PlantCard.svelte
+++ src/components/PlantCard.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { PlantStats } from "../lib/models/plants/Plant";
   import { plantSelector } from "../reactivity/plantSelector.svelte";
+  import { getPlantImage } from "../utils/getPlantImage";
 
   const onSelectPlant = (plantId: string) => {
     plantSelector.selectPlant(plantId);
@@ -15,8 +16,8 @@
 
 <button
   onclick={handleClick}
-  class="flex w-12 flex-col gap-y-2 bg-blue-500 p-4 text-white aspect-square items-center justify-center"
+  class="flex w-16 flex-col p-4 text-white aspect-square items-center justify-center"
 >
-  <p>{name}</p>
+  <img src={getPlantImage(id)} alt={name} class="w-full" />
   <p>{price}</p>
 </button>
