fc9a75dfa57d236d3ac1d8b92723e55080e82d84
oggnimodd <arnold.ds.dev@gmail.com>
2025-01-06 05:10:21 +0000

    style: Adjust GameOptions layout and PlantList styles

--- src/lib/components/GameOptions.svelte
+++ src/lib/components/GameOptions.svelte
@@ -6,7 +6,6 @@
   import PlantCardPreview from "./PlantCardPreview.svelte";
   import Checkbox from "$lib/components/ui/Checkbox.svelte";
   import { onMount } from "svelte";
-  import IconBook from "@tabler/icons-svelte/icons/book-2";
 
   export interface GameOptionsProps {
     startGame: () => void;
@@ -116,9 +115,9 @@
     class="w-full max-w-3xl rounded-lg bg-[#94451C]/90 p-6 shadow-2xl backdrop-blur-sm"
   >
     <!-- Header with Almanac and Back to Menu buttons -->
-    <div class="mb-6 flex items-center justify-between">
+    <div class="mb-4 flex items-center justify-between">
       <h1
-        class="text-center text-3xl font-bold text-lime-400 drop-shadow-[0_2px_2px_rgba(0,0,0,0.5)]"
+        class="text-xl font-bold text-lime-400 drop-shadow-[0_2px_2px_rgba(0,0,0,0.5)] md:text-3xl"
       >
         Game Options
       </h1>
@@ -126,13 +125,12 @@
         onclick={() => cancelGame()}
         class="group flex transform items-center gap-2 rounded-lg border-2 border-lime-400/30 bg-lime-400/10 px-4 py-2 text-lime-400 transition-all hover:border-lime-400/50 hover:bg-lime-400/20 hover:shadow-lg hover:shadow-lime-400/20 active:translate-y-0.5 active:shadow-md"
       >
-        <IconBook class="h-6 w-6" />
         <span class="font-bold">Main Menu</span>
       </button>
     </div>
 
     <!-- Selected Plants Preview -->
-    <div class="mb-6">
+    <div class="mb-4">
       <h2 class="mb-2 text-xl font-semibold text-white">
         Selected Plants ({gameLoop.gameOptions.options.usablePlants.length})
       </h2>
@@ -169,7 +167,7 @@
     </div>
 
     <!-- Available Plants -->
-    <div class="mb-6">
+    <div class="mb-4">
       <h2 class="mb-2 text-xl font-semibold text-white">
         All Plants ({plantSelector.plants.length})
       </h2>
@@ -192,7 +190,7 @@
       </div>
     </div>
 
-    <div class="mb-6">
+    <div class="mb-4">
       <label class="block text-white">
         <span class="mb-1 block text-lg font-medium">Initial Sun Amount:</span>
         <input
--- src/lib/components/PlantList.svelte
+++ src/lib/components/PlantList.svelte
@@ -10,7 +10,9 @@
 </script>
 
 {#if mode === "options"}
-  <div class="flex flex-wrap justify-center gap-2">
+  <div
+    class="sm:max-h-auto grid max-h-[16rem] grid-cols-4 justify-center gap-2 px-2 sm:flex sm:flex-wrap sm:px-0"
+  >
     {@render children()}
   </div>
 {:else if mode === "in-game" || mode === "preview"}
